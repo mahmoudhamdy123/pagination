c959e5c8acfafb6ad1b2713918033e37ddf53297
mahmoudhamdy123 <mh2276419@gmail.com>
2021-08-16 18:09:44 +0200

    clean

--- lib/modules/mainPage/cubit/cubit.dart
+++ lib/modules/mainPage/cubit/cubit.dart
@@ -1,9 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pagination/models/post/post_model.dart';
-
 import 'package:pagination/modules/mainPage/cubit/states.dart';
-import 'package:pagination/shared/network/dio_helper.dart';
 import 'package:pagination/shared/repository/posts_repository.dart';
 
 class PostCubit extends Cubit<PostStates> {
@@ -11,12 +9,10 @@ class PostCubit extends Cubit<PostStates> {
 
   static PostCubit get(context) => BlocProvider.of(context);
 
-  late List<PostModel> _posts;
   int page = 1;
   late final PostsRepository repository = PostsRepository();
 
 
-  List<PostModel> get category => _posts;
 
 
 
@@ -43,24 +39,6 @@ class PostCubit extends Cubit<PostStates> {
   }
 
 
-  Future<void> getPosts(int fetchLimit,[int page = 1]) async {
-    try {
-      emit(GetPostsState());
-      _posts = [];
-      var response = await DioHelper.getData(url: "posts?_limit=$fetchLimit");
-      if (response.statusCode! >= 200 && response.statusCode! <= 299) {
-        var data = response.data;
-        _posts = List<PostModel>.from(
-            data.map((x) => PostModel.fromJson(x)));
-        emit(PostsLoadedState(_posts));
-      } else {
-        emit(ErrorPostsState());
-      }
-    } catch (error) {
-      print(error);
-      emit(ErrorPostsState());
-    }
-  }
 
 
 }
